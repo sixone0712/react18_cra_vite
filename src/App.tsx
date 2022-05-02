c7ba969ae9ca67652e2b1356b6b380ee9aa7badd
Dean <sixone0712@gmail.com>
2022-05-02 23:08:31 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { css } from "@emotion/react";
+import React from "react";
+import "./App.css";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p
+          css={css`
+            color: red;
+          `}
+        >
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
